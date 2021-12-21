c0f26c9fbfdf0ada23fd05f7af7574b1c8093c1f
ozgungnir77 <ozgungnir77@icloud.com>
2021-12-21 17:23:25 +0800

    WeBottomBar布局

--- app/src/main/java/com/ozgungnir77/wecompose/MainActivity.kt
+++ app/src/main/java/com/ozgungnir77/wecompose/MainActivity.kt
@@ -3,20 +3,88 @@ package com.ozgungnir77.wecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ozgungnir77.wecompose.ui.theme.WeComposeTheme
+import com.ozgungnir77.wecompose.ui.theme.black
+import com.ozgungnir77.wecompose.ui.theme.green3
+import com.ozgungnir77.wecompose.ui.theme.white1
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             WeComposeTheme {
-                
+                Column {
+                    WeBottomBar(selected = 0)
+                }
             }
         }
     }
+}
+
+@Composable
+private fun WeBottomBar(selected: Int) {
+    Row(Modifier.background(white1)) {
+        TabItem(
+            if (selected == 0) R.drawable.ic_chat_filled else R.drawable.ic_chat_outlined,
+            "聊天",
+            if (selected == 0) green3 else black,
+            Modifier.weight(1f)
+        )
+        TabItem(
+            if (selected == 1) R.drawable.ic_contacts_filled else R.drawable.ic_contacts_outlined,
+            "通讯录",
+            if (selected == 1) green3 else black,
+            Modifier.weight(1f)
+        )
+        TabItem(
+            if (selected == 2) R.drawable.ic_discovery_filled else R.drawable.ic_discovery_outlined,
+            "发现",
+            if (selected == 2) green3 else black,
+            Modifier.weight(1f)
+        )
+        TabItem(
+            if (selected == 3) R.drawable.ic_me_filled else R.drawable.ic_me_outlined,
+            "我",
+            if (selected == 3) green3 else black,
+            Modifier.weight(1f)
+        )
+    }
+}
+
+@Composable
+private fun TabItem(
+    @DrawableRes iconId: Int,
+    title: String,
+    tint: Color,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier.padding(vertical = 8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(painterResource(iconId), title, Modifier.size(24.dp), tint = tint)
+        Text(title, fontSize = 11.sp, color = tint)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WeBottomBarPreview() {
+    WeBottomBar(selected = 0)
 }
