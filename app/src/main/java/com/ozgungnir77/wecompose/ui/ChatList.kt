6fba5c70740ecb80f0ad99672b40ff56e7fad608
ozgungnir77 <ozgungnir77@icloud.com>
2021-12-23 16:11:31 +0800

    chat message layout

--- app/src/main/java/com/ozgungnir77/wecompose/ui/ChatList.kt
+++ app/src/main/java/com/ozgungnir77/wecompose/ui/ChatList.kt
@@ -1,27 +1,68 @@
 package com.ozgungnir77.wecompose.ui
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ozgungnir77.wecompose.data.Chat
+import com.ozgungnir77.wecompose.ui.theme.WeComposeTheme
 
 @Composable
 fun ChatList(chats: List<Chat>) {
-    LazyColumn {
+    LazyColumn(Modifier.fillMaxSize()) {
         items(chats) { chat ->
-            Image(
-                painterResource(chat.friend.avatar), contentDescription = chat.friend.name,
-                Modifier
-                    .size(48.dp)
-                    .clip(RoundedCornerShape(4.dp))
-            )
+            Row(Modifier.fillMaxWidth()) {
+                Image(
+                    painterResource(chat.friend.avatar), chat.friend.name,
+                    Modifier
+                        .padding(8.dp)
+                        .size(48.dp)
+                        .unread(!chat.msgs.last().read, WeComposeTheme.colors.badge),
+                )
+                Column(
+                    Modifier
+                        .weight(1f)
+                        .align(Alignment.CenterVertically),
+                ) {
+                    Text(
+                        chat.friend.name,
+                        fontSize = 17.sp,
+                        color = WeComposeTheme.colors.textPrimary
+                    )
+                    Text(
+                        chat.msgs.last().text,
+                        fontSize = 14.sp,
+                        color = WeComposeTheme.colors.textSecondary
+                    )
+                }
+                Text(
+                    chat.msgs.last().time,
+                    Modifier.padding(8.dp, 8.dp, 12.dp, 8.dp),
+                    fontSize = 11.sp,
+                    color = WeComposeTheme.colors.textSecondary
+                )
+            }
         }
+    }
+}
+
+fun Modifier.unread(show: Boolean, color: Color): Modifier = this.drawWithContent {
+    drawContent()
+    if (show) {
+        drawCircle(
+            color,
+            5.dp.toPx(),
+            Offset(size.width - 1.dp.toPx(), 1.dp.toPx())
+        )
     }
 }
